5c2d538696af578090647fd9b8e97e88034acd59
JingoBongo <justfortoma1607>
2021-05-09 23:29:53 +0300

    final exe ready, baby!! icons are here, text size and console background are there. cycled shmoys is here

--- pyqt5-version-2/img-to-bytes.py
+++ pyqt5-version-2/img-to-bytes.py
@@ -1,12 +1,16 @@
 import base64
 
-
-with open("icon.png", "rb") as image2string:
-    converted_string = base64.b64encode(image2string.read())
-print(converted_string)
+def pngtostring(filename):
+    with open(filename, "rb") as image2string:
+        converted_string = base64.b64encode(image2string.read())
+    print(converted_string)
+    return converted_string
 
 # =================
-
-decodeit = open('newfile.png', 'wb')
-decodeit.write(base64.b64decode((converted_string)))
-decodeit.close()
+def stringtopng(converted_string):
+    # decodeit = open('newfile.png', 'wb')
+    # decodeit.write(base64.b64decode((converted_string)))
+    # decodeit.close()
+    return base64.b64decode((converted_string))
+pngtostring('icon.png')
+pngtostring('exit.png')
